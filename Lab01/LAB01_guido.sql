cd490c7c273b881ba4d7568197fe0ad1fec80ad0
guidox18 <guido_x18@hotmail.com>
2016-05-08 16:08:53 -0500

    Actividad 05 - lab01
    script del 1 al  4

--- Lab01/LAB01_guido.sql
+++ Lab01/LAB01_guido.sql
@@ -58,12 +58,54 @@ sola y única columna, los datos deben estar separados por
 una coma y la columna debe tener como encabezado Los Empleados*/
 
 SELECT (FIRST_NAME + ', ' + LAST_NAME+ ', ' + EMAIL + ', ' 
-+PHONE_NUMBER + ', ' + HIRE_DATE+ ', ' + JOB_ID+ ', ' + SALARY+ ', ' 
-+ COMMISSION_PCT+ ', ' + MANAGER_ID + ', ' +DEPARTMENT_ID) As 'Los Empleados' FROM HR.EMPLOYEES
++PHONE_NUMBER + ', ' + CONVERT(varchar(10), HIRE_DATE) + ', ' + JOB_ID + ', ' + CONVERT(varchar(10), SALARY)+ ', ' 
++  CONVERT(varchar(10), COMMISSION_PCT)+ ', ' +  CONVERT(varchar(10), MANAGER_ID) + ', ' + DEPARTMENT_ID) As 'Los Empleados' FROM HR.EMPLOYEES
 
 
 /*Actividad N° 05 
 – Funciones*/
-/*Se requiere realizar una consulta que visualice la fecha del sistema.*/
+/*1.Se requiere realizar una consulta que visualice la fecha del sistema.*/
 Select GETDATE()
 
+/*2.El departamento de Recursos Humanos necesita un reporte de todos los empleados que muestre 
+el N° de Empleado,  Apellidos, Salario y una columna más con el cálculo del salario incrementado en 15.5% 
+(expresado solo en enteros) esta columna debe etiquetarse Nuevo Salario. */
+SELECT 
+EMPLOYEE_ID as 'Nº de empleado'
+,LAST_NAME as 'Apellidos'
+,SALARY as 'Salario'
+,CONVERT(integer, SALARY + (15.5 * SALARY / 100 ))  as 'Nuevo Salario' 
+FROM HR.EMPLOYEES
+
+/*3.Modificar  la  consulta  anterior  y  adicionar  una  columna  que  
+muestre  el  resultado  de  la  resta  entre  el antiguo salario y el 
+nuevo salario. Etiquetar esta columna como Incremento.*/
+SELECT 
+EMPLOYEE_ID as 'Nº de empleado'
+,LAST_NAME as 'Apellidos'
+,SALARY as 'Salario'
+,CONVERT(integer, SALARY + (15.5 * SALARY / 100 ))  as 'Nuevo Salario' 
+,CONVERT(integer, SALARY - (SALARY + (15.5 * SALARY / 100 )))  as 'Incremento' 
+FROM HR.EMPLOYEES
+/*4.Crear  un  reporte  que  muestre  los  Apellidos  (con  la  primera  letra  en  Mayúsculas  y  las  demás  en 
+Minúsculas)  y  la  longitud  de  los  apellidos  (colocar  alias  Longitud),  para  todos  aquellos  empleados 
+quienes sus apellidos empiecen con las letras ‘J’, ‘A’ y ‘M’. Ordenar los resultados por la columna 
+Apellido.*/
+SELECT 
+CONCAT(
+	UPPER(LEFT(FIRST_NAME,1))
+	,
+	LOWER(SUBSTRING(FIRST_NAME,2,null))
+) as Apellidos 
+,LEN(FIRST_NAME) as Longitud
+FROM  HR.EMPLOYEES
+WHERE
+	LEFT(FIRST_NAME,1) = 'J' or
+	LEFT(FIRST_NAME,1) = 'A'  or 
+	LEFT(FIRST_NAME,1) = 'M' 
+ORDER BY FIRST_NAME 
+
+
+
+
+ 
